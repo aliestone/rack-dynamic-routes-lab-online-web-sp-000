2a1c30986f04a21a648ead7f9ace0b7811c20b51
Alie Stone <helloaliestone@gmail.com>
2020-12-15 20:02:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,12 +4,12 @@ class Application
     def call(env)
       resp = Rack::Response.new
       req = Rack::Request.new(env)
-  
+
       if req.path.match(/items/)
         item_name = req.path.split("/items/").last
         if item = @@items.find{|i| i.name == item_name}
           resp.write item.price
-        else 
+        else
           resp.status = 400
           resp.write "Item not found"
         end
@@ -19,5 +19,5 @@ class Application
       end
       resp.finish
     end
-  
+
   end
